6f0ed25684e353e7b74294564d0aa5f206b86fd2
msm <myatsumon@ucsy.edu.mm>
2024-02-17 16:19:08 +0630

    feat: auth middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <main className="text-sky-400">hello world
-    <Button variant='destructive'>click me</Button></main>
-  );
+  return <main className="text-sky-400">protected page</main>;
 }
